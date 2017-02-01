d7380cd2498d6c78ce90c96c54b4ee1fda6d9c39
Giovanni Di Mauro <giovannidm80@gmail.com>
2017-02-01 09:35:58 +0100

    Create function.php

---
+++ function.php
@@ -0,0 +1,11 @@
+//da inserire nel file function.php
+<?php
+//apre link su nuova scheda inizio 
+function apri_in_nuova_scheda($text) {
+	$return_url = str_replace('<a', '<a target="_blank" title="Apri link su nuova scheda"', $text);
+	return $return_url;
+}
+add_filter('get_comment_author_link', 'apri_in_nuova_scheda');
+add_filter('comment_text', 'apri_in_nuova_scheda');
+//apre link su nuova scheda fine
+?>
